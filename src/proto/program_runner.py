edd63c382b554838dc5e3950c6746a2025efdb5b
Fodor Kis, János <janos.fodorkis@thermofisher.com>
2020-04-22 11:09:08 +0200

    Added proto Main Program

---
+++ src/proto/program_runner.py
@@ -0,0 +1,12 @@
+def setup(v):
+    print("In the main function with value {}".format(v))
+
+
+def main():
+    setup(13)
+    print('Hello from main')
+
+
+if __name__ == "__main__":
+    main()
+    print('Hello from the current module entry point')
